2b405843c3a33d9e6031153da7a30e0c1f40ba67
Chris Cankov <cankov.chris@gmail.com>
2017-04-05 23:01:59 -0700

    Implement dominant octopus to find fish in O(nlogn) time

--- w2d4/octopus_problems.rb
+++ w2d4/octopus_problems.rb
@@ -10,3 +10,35 @@ def sluggish_octopus(fish)
     return fish1 if longest_fish
   end
 end
+
+def dominant_octopus(fish)
+
+  def mergesort(arr, &prc)
+    return arr if arr.length < 2
+    prc ||= Proc.new { |x, y| x <=> y }
+    midpoint = arr.length / 2
+    left = mergesort(arr[0...midpoint], &prc)
+    right = mergesort(arr[midpoint...arr.length], &prc)
+    merge(left, right, &prc)
+  end
+
+  def merge(left, right, &prc)
+    merged = []
+    until left.empty? || right.empty?
+      if (prc.call(left.first, right.first) == 1)
+        merged << right.shift
+      else
+        merged << left.shift
+      end
+    end
+    merged + left + right
+  end
+
+  sorted_fish = mergesort(fish) { |fish1, fish2| fish1.length <=> fish2.length }
+
+  sorted_fish.last
+end
+
+
+p sluggish_octopus(FISH) == 'fiiiissshhhhhh'
+p dominant_octopus(FISH) == 'fiiiissshhhhhh'
