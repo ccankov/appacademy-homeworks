424afbe1a3b055975114678fd4efd5dbac940465
Chris Cankov <cankov.chris@gmail.com>
2017-04-20 17:21:00 -0700

    Complete integration tests

---
+++ w4d5/MusicApp/spec/features/auth_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature "the signup process" do
+
+  scenario "has a new user page" do
+    visit new_user_url
+    expect(page).to have_content "New User"
+  end
+
+  feature "signing up a user" do
+    before(:each) do
+      visit new_user_url
+      fill_in 'Email', :with => "testing@email.com"
+      fill_in 'Password', :with => "biscuits"
+      click_on "create user"
+    end
+
+    scenario "redirects to new session page after signup" do
+      save_and_open_page
+      expect(current_path).to eq('/session/new')
+    end
+  end
+
+  feature "with an invalid user" do
+    before(:each) do
+      visit new_user_url
+      fill_in 'Email', :with => "testing@email.com"
+      click_on "create user"
+    end
+
+    scenario "re-renders the signup page after failed signup" do
+      expect(page).to have_content('New User')
+    end
+  end
+
+end
