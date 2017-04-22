26dbff25df653fe69f99120ee155179b6035f6d8
Chris Cankov <cankov.chris@gmail.com>
2017-04-22 08:49:15 -0700

    Create polymorphic associations for Toy model; w5d1

--- w5d1/ToyApp/app/models/toy.rb
+++ w5d1/ToyApp/app/models/toy.rb
@@ -1,2 +1,6 @@
 class Toy < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: [:toyable_id, :toyable_type] }
+
+  belongs_to :toyable, polymorphic: true
 end
