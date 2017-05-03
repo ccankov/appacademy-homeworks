3d093751c093555fca6b75e586358821571e9d0c
Chris Cankov <cankov.chris@gmail.com>
2017-05-02 22:55:14 -0700

    Complete w6d3: first ajax request

--- w6d3/first-ajax/js/index.js
+++ w6d3/first-ajax/js/index.js
@@ -1,5 +1,12 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax({
+  method: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b', // eslint-disable-line max-len
+  success: function success(weather) {
+    console.log(weather);
+  }
+});
 // Add another console log here, outside your AJAX request
+console.log("I will beat the AJAX console.log()!");
