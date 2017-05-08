c0ac924c28eb8f7411c53ebff27400dbb3cef2ce
Chris Cankov <cankov.chris@gmail.com>
2017-05-07 20:13:43 -0700

    Start w7d1; implement store and actions

---
+++ w7d1/dollar-store/frontend/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  baseCurrency: "Please select",
+  rates: {}
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SWITCH_CURRENCY':
+      
+      break;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
