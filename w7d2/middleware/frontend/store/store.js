eaac85ebdbe1c0d15105b88cfe1b498c599e9dcc
Chris Cankov <cankov.chris@gmail.com>
2017-05-08 20:05:25 -0700

    Refactor code to use built-in applyMiddleware functunction and refactor logger to use it

--- w7d2/middleware/frontend/store/store.js
+++ w7d2/middleware/frontend/store/store.js
@@ -1,12 +1,25 @@
 import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  let result = next(action);
+  console.log(store.getState());
+  return result;
+};
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(RootReducer, preloadedState);
+  const store = createStore(
+    RootReducer,
+    preloadedState,
+    applyMiddleware(addLoggingToDispatch)
+  );
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
-}
+};
 
 export default configureStore;
--- w7d2/middleware/frontend/todo_redux.jsx
+++ w7d2/middleware/frontend/todo_redux.jsx
@@ -4,26 +4,10 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const applyMiddlewares = (store, middlewares) => {
-  let dispatch = store.dispatch;
-  middlewares.forEach((middleware) => {
-    dispatch = middleware(store)(dispatch);
-  });
-  return Object.assign({}, store, { dispatch });
-};
-
-const addLoggingToDispatch = store => next => action => {
-  console.log(store.getState());
-  console.log(action);
-  store.dispatch(action);
-  console.log(store.getState());
-};
-
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   let store = configureStore(preloadedState);
-  store = applyMiddlewares(store, [addLoggingToDispatch]);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
