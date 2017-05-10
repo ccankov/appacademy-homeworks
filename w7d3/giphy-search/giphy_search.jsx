5038c5901fbb96157a16ad75138e06d77840864b
Chris Cankov <cankov.chris@gmail.com>
2017-05-09 21:00:56 -0700

    Complete actions, include thunk middleware, use provider

--- w7d3/giphy-search/giphy_search.jsx
+++ w7d3/giphy-search/giphy_search.jsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-  window.store = store;
-  window.receiveSearchGiphys = receiveSearchGiphys;
-  window.fetchSearchGiphys = fetchSearchGiphys;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>An App</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
